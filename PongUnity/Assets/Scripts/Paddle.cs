8cb93ddb458343605e0b4858067bb77ebfcdc919
DerpidiusBidoof <82119250+DerpidiusBidoof@users.noreply.github.com>
2023-08-23 15:52:42 -0500

    Stubbed out code for paddle and declared paddle variables

---
+++ PongUnity/Assets/Scripts/Paddle.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Paddle : MonoBehaviour
+{
+    public float verticalMovementSpeed;
+    public float maxYPosition;
+    public KeyCode upKey;
+    public KeyCode downKey;
+    public int ballHitXDirection;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MoveUp()
+    {
+
+    }
+
+    public void MoveDown()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+}
